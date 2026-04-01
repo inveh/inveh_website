51da1ff0410394c6e8ec7a79b0066e1f6cbb9f04
inveh <inveh.info@gmail.com>
2026-04-01 16:01:55 +0530

    contact page frozen

---
+++ src/Contact.vue
@@ -0,0 +1,127 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <div class="description_style">
+    <ul>
+      <li>We are passionate engineers from a small town in India with a Glocalization idea, working to address the gap between the technology and comfort.</li>
+      <li>In 2023, it all started as a hobby project, slowly acquiring wings to fly as a small manufacturing unit catering the demands.</li>
+      <li>In the products page, you shall see the list of products which we already supply the market.</li>
+      <li>Feel free to contact us for your personalized Lighting Solutions.</li>
+      <br />
+      <li>Happy Lighting!</li>
+    </ul>
+  </div>
+
+  <div class="legaldetail_style">
+    <h3>Legal Registration Details</h3><br />
+    <p>GST: GSTIN 33JUHPK5441R1ZX</p> <br />
+    <p>MSME: UDYAM-TN-28-0165965</p> <br />
+    <p>Subject to Udumalpet Jurisdiction</p>
+  </div>
+
+  <div class="contactContent_style">
+    <h3>Reach us at:</h3><br />
+    <p>✉ <a href="mailto:info@inveh.in">info@inveh.in</a></p><br />
+    <p>☎ +91 94877 41183</p>
+  </div>
+
+  <div class="legaldetail_style">
+    <h3>Visit us at:</h3><br />
+    <p>Inveh Lighting Solutions,</p><br />
+    <p>10A1, Poompugar nagar, Dhali road,</p><br />
+    <p>Udumalpet - 642 154,</p><br />
+    <p>Tamilnadu, India.</p>
+  </div>
+
+  <div class="map-widget">
+    <iframe
+      title="Inveh Lighting Solutions Location"
+      src="https://www.google.com/maps?q=Inveh%20Lighting%20Solutions%2C%20Udumalpet%2C%20Tamil%20Nadu%2C%20India&output=embed"
+      width="350"
+      height="350"
+      style="border:0;"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
+</template>
+
+
+<style scoped>
+* {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif;
+}
+
+.description_style {
+  text-align: center;
+  max-width: 1500px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+
+.description_style ul {
+  list-style-position: inside;
+  padding-left: 0;
+}
+
+.description_style li {
+  margin-bottom: 1rem;
+  line-height: 1;
+}
+
+.legaldetail_style {
+  text-align: left;
+  margin: 0 auto;
+  max-width: 800px;
+  line-height: 1;
+}
+
+.legaldetail_style h3 {
+  margin-bottom: 0.5rem;
+}
+
+.legaldetail_style p {
+  margin: 0.25rem 0;
+  display: inline-block;
+  margin-right: 2rem;
+}
+
+.contactContent_style {
+  text-align: right;
+  margin-left: auto;
+  margin-right: 40rem;
+  max-width: 800px;
+  line-height: 1;
+}
+
+.contactContent_style h3 {
+  margin-bottom: 0;
+}
+
+.contactContent_style p {
+  margin: 0 0;
+  display: inline-block;
+  margin-left: 2rem;
+}
+
+.contactContent_style a {
+  color: #0A66C2;
+  text-decoration: none;
+}
+
+.contactContent_style a:hover {
+  text-decoration: underline;
+}
+
+.map-widget {
+  max-width: 350px;
+  margin: 0 auto 2rem auto;
+}
+
+.map-widget iframe {
+  display: block;
+  margin: 0 auto;
+}
+</style>
