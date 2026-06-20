7c95d20dcb4ef3d1dcfb023cdb17de6e1b9b8040
inveh <inveh.info@gmail.com>
2026-06-20 07:58:52 +0530

    one liner set

--- src/Layout.vue
+++ src/Layout.vue
@@ -14,7 +14,7 @@ const openCart = () => {
   <div class="layout-wrapper">
     <!-- Announcement Bar -->
     <div class="announcement-bar">
-      <span>Welcome to Inveh Lighting Solutions</span>
+      <span>We craft premium, vintage wooden LED lamps.</span>
     </div>
 
     <!-- Main Header -->
